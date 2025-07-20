00cd10151d2ec56665fb4a16d8e737640fefe5f9
abdulsalamkz <abdulsalamkhanzada@gmail.com>
2025-07-20 23:40:58 +0500

    Created Table with API

---
+++ fetch.js
@@ -0,0 +1,35 @@
+async function getData() {
+  const response = await fetch("../JSon/hello.json");
+  const data = await response.json();
+  for (const key in data[0]) {
+    const th = document.createElement("th");
+    headRow.appendChild(th);
+    th.innerHTML = key;
+  }
+
+  data.forEach((element) => {
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+    for (const key in element) {
+      const td = document.createElement("td");
+      tr.appendChild(td);
+      td.innerHTML = element[key];
+    }
+  });
+}
+
+const body = document.getElementById("test");
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+body.appendChild(table);
+table.appendChild(thead);
+table.appendChild(tbody);
+
+const headRow = document.createElement("tr");
+thead.appendChild(headRow);
+const th = document.createElement("th");
+
+table.appendChild(tbody);
+
+getData();
